b1258f3309ba2fcebfb1b02a1ce9017bc68e2b7d
Rebecca Chiquete <deerinra194@gmail.com>
2021-03-25 12:53:19 -0700

    push

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,75 +1,90 @@
-const APP_PREFIX = "my-cache",
-const VERSION = "v1",
-const CACHE_NAME = APP_PREFIX+VERSION,
-const DATA_CACHE_NAME = "data-cache-" + VERSION
-
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
 
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/style.css',
-    '/js/idb.js',
-    '/js/index.js',
+    '/db.js',
+    '/index.js',
     '/manifest.json',
     '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
+    '/icons/icon-512x512.png'
   ];
-  
-  
-  self.addEventListener('install', (event) => {
+
+  self.addEventListener("install", event => {
     event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then(function(cache){
-                console.log("installing" + CACHE_NAME)
-                return cache.addAll(FILES_TO_CACHE)
-        })
+        caches
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
+});
+
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
     event.waitUntil(
-      caches
-        .keys()
-        .then(function(keylist){
-            var cachekeep = keylist.filter(function(key){
-                return key.indexOf(APP_PREFIX)
+        caches
+            .keys()
+            .then(cacheNames => {
+                // return array of cache names that are old to delete
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName)
+                );
             })
-            cachekeep.push(CACHE_NAME)
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                  console.log('deleting cache : ' + keyList[i] );
-                  return caches.delete(keyList[i]);
-                }
-              }));
-        })
+            .then(cachesToDelete => {
+                return Promise.all(
+                    cachesToDelete.map(cacheToDelete => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
     );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes("/api")){
-        event.respondwith(
-            caches.open(DATA_CACHE_NAME).then(function(){
-                return fetch(event.request).then(function(response){
-                    if (response.status === 200){
-                        cache.put(event.request.url, response.clone())
-                    }
-                    return response
-                })
-            })
-        )
+});
+self.addEventListener("fetch", event => {
+    // non GET requests are not cached and requests to other origins are not cached
+    if (
+        event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
     }
-    event.respondwith(
-        fetch(event.request).catch(function(){
-            return caches.match(event.request).then(function(response) {
-                if (response) {
-                  return response;
-                } else if (event.request.headers.get("accept").includes("text/html")) {
-                  return caches.match("/");
-                }
+    // handle runtime GET requests for data from /api routes
+    if (event.request.url.includes("/api")) {
+        // make network request and fallback to cache if network request fails (offline)
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(event.request));
             })
+        );
+        return;
+    }
+    // use cache first for all other requests for performance
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            else if (event.request.headers.get("accept").includes("text/html")) {
+                // return the cached home page for all requests for html pages
+                return caches.match("/");
+            }
+            // request is not in cache. make network request and cache the response
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
         })
-    )
-  });
+    );
+});
   
